13ba09ff94b44cb48e1c7b1f3852acf6ed3243d4
James Deaton <jamesdeaton72@gmail.com>
2025-09-26 15:46:15 -0500

    Add files via upload

--- src/main/java/net/milkbowl/vault/economy/EconomyResponse.java
+++ src/main/java/net/milkbowl/vault/economy/EconomyResponse.java
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> dc4117b0ef9ec12b687958904b6d08ab3dde342f
 package net.milkbowl.vault.economy;
 
 import org.jetbrains.annotations.NotNull;
@@ -55,49 +51,4 @@ public class EconomyResponse
     } 
      return false;
   }
-<<<<<<< HEAD
-=======
-=======
-package net.milkbowl.vault.economy;
-/*    */
-public class EconomyResponse
-{
-  public final double amount;
-  public final double balance;
-  public final ResponseType type;
-  public final String errorMessage;
-  
-  public enum ResponseType
-  {
-     SUCCESS(1),
-     FAILURE(2),
-     NOT_IMPLEMENTED(3);
-    
-    private int id;
-    
-    ResponseType(int id) {
-       this.id = id;
-    }
-    
-    int getId() {
-       return this.id;
-    }
-  }
-/*    */
-  public EconomyResponse(double amount, double balance, ResponseType type, String errorMessage) {
-     this.amount = amount;
-     this.balance = balance;
-     this.type = type;
-     this.errorMessage = errorMessage;
-  }
-/*    */
-  public boolean transactionSuccess() {
-     switch (this.type) {
-      case SUCCESS:
-         return true;
-    } 
-     return false;
-  }
->>>>>>> 2428b33fef20d64affb4890e28cad1bbccb9071c
->>>>>>> dc4117b0ef9ec12b687958904b6d08ab3dde342f
 }
